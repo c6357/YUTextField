be20827bd9ae2ed623bb78eda8f3798b120e4fc1
6357 <c6357@outlook.com>
2015-04-28 21:30:28 +0800

    Initial Commit

---
+++ YUTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YUTextField
+//
+//  Created by yuzhx on 15/4/28.
+//  Copyright (c) 2015年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
